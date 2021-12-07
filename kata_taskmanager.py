22c94ef526a2374ff7b10757e3257a1f5c076ead
BouleO <43885816+BouleO@users.noreply.github.com>
2021-12-07 15:49:03 +0100

    parsing : remove task

--- kata_taskmanager.py
+++ kata_taskmanager.py
@@ -2,7 +2,9 @@ class Parser:
 
     def parse(query):
         args = query.split(" ",1)
-        if(args[0] == "+" and args[1]):
+        if (args[0] == "+" and args[1]):
             return "adding task : " + args[1]
+        elif (args[0] == "-" and args[1].isdigit()):
+            return "removing task number " + args[1]
         else:
             return "parsing error"
--- test_kata_taskmanager.py
+++ test_kata_taskmanager.py
@@ -4,3 +4,6 @@ def test_parse_add():
     result = Parser.parse("+ Learn Python")
     assert result == "adding task : Learn Python"
 
+def test_parse_remove():
+    result = Parser.parse("- 1")
+    assert result == "removing task number 1"
